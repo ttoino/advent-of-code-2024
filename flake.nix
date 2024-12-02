a705754f023868c46300e5ffe0a406734210898d
toino <me@toino.pt>
2024-12-02 01:22:34 +0100

    Automatically get input

--- flake.nix
+++ flake.nix
@@ -8,14 +8,40 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+
+      getInput = pkgs.writeShellApplication {
+        name = "getInput";
+        runtimeInputs = [ pkgs.curl ];
+        text = ''
+          CACHE_DIR="''${XDG_CACHE_HOME:-$HOME/.cache}/aoc2024"
+          mkdir -p "$CACHE_DIR"
+          SESSION_FILE="$CACHE_DIR/session"
+          OUTPUT_FILE="$CACHE_DIR/day$1"
+
+          while [ ! -f "$OUTPUT_FILE" ]; do
+            if [ ! -f "$SESSION_FILE" ]; then
+              echo -n "Please enter your session cookie: " >&2
+              read -r SESSION
+              echo "$SESSION" > "$SESSION_FILE"
+            fi
+
+            if ! curl "https://adventofcode.com/2024/day/$1/input" -sf -H "Cookie: session=$(cat "$SESSION_FILE")" -o "$OUTPUT_FILE"; then
+              echo "Invalid session cookie" >&2
+              rm -f "$SESSION_FILE" "$OUTPUT_FILE"
+            fi
+          done
+
+          cat "$OUTPUT_FILE"
+        '';
+      };
     in
     {
       packages.${system} = {
         day1 = pkgs.writeShellApplication {
           name = "day1";
-          runtimeInputs = [ (pkgs.dyalog.override { acceptLicense = true; }) ];
-          text = "dyalog -script ${./day1.apl}";
+          runtimeInputs = [ (pkgs.dyalog.override { acceptLicense = true; }) getInput ];
+          text = "getInput 1 | dyalog -script ${./day1.apl} 2> /dev/null";
         };
       };
     };
