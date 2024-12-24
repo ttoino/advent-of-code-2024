0d1501058de8518031da446e6385526fb4081d41
toino <me@toino.pt>
2024-12-24 21:26:24 +0000

    Initial day 16 solution with dijkstra

--- day16.pl
+++ day16.pl
@@ -1,4 +1,5 @@
-:- use_module(library(clpfd)).
+:- use_module(library(heaps)).
+:- use_module(library(ordsets)).
 
 get_input(Input) :-
     read_line_to_codes(user_input, Line),
@@ -7,104 +8,47 @@ get_input(Input) :-
         get_input(Others), Input = [Line | Others]
     ).
 
-setup_el(_, _, [], [], []).
-setup_el(
-    West,
-    [North | NextPrevSouths],
-    [Code | NextCodes],
-    [Code-North-West-South-East | NextGraphs],
-    [South | NextSouths]
-) :-
-    [North, West, South, East] ins 0..1,
-    setup_el(East, NextPrevSouths, NextCodes, NextGraphs, NextSouths).
+nnth0(Row-Col, Graph, El) :-
+    nth0(Row, Graph, RowStr),
+    nth0(Col, RowStr, El).
 
-setup_line(_, [], []).
-setup_line(PrevSouths, [Line | NextLines], [GraphLine | NextGraphs]) :-
-    setup_el(_, PrevSouths, Line, GraphLine, Souths),
-    setup_line(Souths, NextLines, NextGraphs).
-
-setup_graph([I | Input], Graph) :-
-    same_length(I, Souths),
-    setup_line(Souths, [I | Input], Graph).
-
-setup_el_constraints([], []).
-setup_el_constraints([0'#-North-West-South-East | Graph], [0 | Scores]) :- !,
-    sum([North, West, South, East], #=, 0),
-    setup_el_constraints(Graph, Scores).
-setup_el_constraints([0'S-North-West-South-East | Graph], [Score | Scores]) :- !,
-    sum([North, West, South, East], #=, 1),
-    East #= 1 #<==> Score #= 0,
-    East #= 0 #<==> Score #= 1000,
-    setup_el_constraints(Graph, Scores).
-setup_el_constraints([0'E-North-West-South-East | Graph], [1 | Scores]) :- !,
-    sum([North, West, South, East], #=, 1),
-    setup_el_constraints(Graph, Scores).
-setup_el_constraints([0'.-North-West-South-East | Graph], [Score | Scores]) :- !,
-    sum([North, West, South, East], #=, Sum),
-    Sum #= 0 #\/ Sum #= 2,
-    Sum #= 0 #==> Score #= 0,
-    North + South #= 2 #\/ West + East #= 2 #<==> Parallel,
-    Sum #= 2 #/\ Parallel #==> Score #= 1,
-    Sum #= 2 #/\ #\ Parallel #==> Score #= 1001,
-    setup_el_constraints(Graph, Scores).
-setup_el_constraints([Code-_-_-_-_ | Graph], [0 | Scores]) :- !,
-    char_code(Atom, Code),
-    write('Invalid character: '), write(Atom), write('('), write(Code), write(')'), nl,
-    setup_el_constraints(Graph, Scores).
-    
-setup_line_constraints([], []).
-setup_line_constraints([Line | Graph], [ScoreLine | Scores]) :-
-    setup_el_constraints(Line, ScoreLine),
-    setup_line_constraints(Graph, Scores).
-
-line_variables([], []).
-line_variables([_-North-West-South-East | Line], [North, West, South, East | LineVars]) :-
-    line_variables(Line, LineVars).
-variables([], []).
-variables([Line | Graph], [LineVars | GraphVars]) :-
-    line_variables(Line, LineVars),
-    variables(Graph, GraphVars).
-
-print_down([]) :- nl.
-print_down([_-_-_-South-_ | Line]) :-
+add_dirs(_, Heap, _, _, [], Heap).
+add_dirs(Graph, Heap, Distance, Row-Col-Dir, [RowD-ColD-DirD | Dirs], FinalHeap) :-
     (
-        \+ ground(South), !, write('?');
-        South = 1, !, write('|');
-        South = 0, !, write(' ')
+        Dir = DirD -> NewDistance is Distance + 1;
+        NewDistance is Distance + 1001
     ),
-    write(' '),
-    print_down(Line).
-print_line([]) :- nl.
-print_line([Code-_-_-_-East]) :-
-    char_code(Atom, Code), write(Atom), nl.
-print_line([Code-_-_-_-East | Line]) :-
-    char_code(Atom, Code), write(Atom),
+    NewRow is Row + RowD,
+    NewCol is Col + ColD,
     (
-        \+ ground(East), !, write('?');
-        East = 1, !, write('-');
-        East = 0, !, write(' ')
+        nnth0(NewRow-NewCol, Graph, 0'#) -> NewHeap = Heap;
+        add_to_heap(Heap, NewDistance, NewRow-NewCol-DirD, NewHeap)
     ),
-    print_line(Line).
-print_graph([]).
-print_graph([Line]) :- print_line(Line).
-print_graph([Line | Graph]) :-
-    print_line(Line),
-    print_down(Line),
-    print_graph(Graph).
+    add_dirs(Graph, NewHeap, Distance, Row-Col-Dir, Dirs, FinalHeap).
+
+dijkstra(Heap, Visited, Graph, Distance) :-
+    % heap_size(Heap, Size),
+    % write(Size), nl,
+    get_from_heap(Heap, CurrentDistance, Row-Col-Dir, NewHeap),
+    (
+        ord_memberchk(Row-Col, Visited) -> dijkstra(NewHeap, Visited, Graph, Distance);
+
+        ord_add_element(Visited, Row-Col, NewVisited),
+        nth0(Row, Graph, R),
+        nth0(Col, R, El),
+        (
+            El = 0'E -> Distance = CurrentDistance;
+
+            add_dirs(Graph, NewHeap, CurrentDistance, Row-Col-Dir, [0-1-east, 0-(-1)-west, 1-0-south, (-1)-0-north], FinalHeap),
+            dijkstra(FinalHeap, NewVisited, Graph, Distance)
+        )
+    ).
 
 part1(Input, Output) :-
-    setup_graph(Input, Graph),
-    print_graph(Graph),
-    setup_line_constraints(Graph, Scores),
-    print_graph(Graph),
-    append(Scores, ScoresFlat),
-    sum(ScoresFlat, #=, Output),
-    variables(Graph, Vars),
-    append(Vars, VarsFlat),
-    !,
-    write('Labeling'), nl,
-    labeling([min(Output), ff], VarsFlat),
-    print_graph(Graph).
+    nnth0(SR-SC, Input, 0'S),
+    singleton_heap(Heap, 0, SR-SC-east),
+    ord_empty(Visited),
+    dijkstra(Heap, Visited, Input, Output).
 
 part2(_Input, Output) :-
     Output is 0.
